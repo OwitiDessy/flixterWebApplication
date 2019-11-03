712842f711e7830f4a4ddda1f447a29959b1dd2b
Dessy Owiti <owitidessy@yahoo.com>
2019-11-03 17:43:48 +0000

    Lesson 28 permissions

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,16 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+    before_action :require_authorized_for_current_lesson.section.course, only: [:show]
   def show
   end
 
   private
+
+  def require_authorized_for_current_lesson.section.course
+    if current_lesson.section.course.user != current_user
+      redirect_to course_path, alert: 'Error Message Here'
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
